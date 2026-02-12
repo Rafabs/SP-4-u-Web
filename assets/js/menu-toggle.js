d5581281f2d5d82c8dbbd7bc8c01f28baffc392f
Rafabs <rafael_barbosa2@hotmail.com>
2026-02-12 01:23:54 -0300

    main
    12/02/2026 - v1.0.0.0 - Desenvolvido funcionalidade para subir para o topo da página (Back to Top)

--- assets/js/menu-toggle.js
+++ assets/js/menu-toggle.js
@@ -3,15 +3,37 @@ document.addEventListener("DOMContentLoaded", () => {
   const navMenu = document.getElementById("nav-menu");
 
   if (menuToggle) {
-      menuToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
-    menuToggle.classList.toggle("open"); 
-  });
+    menuToggle.addEventListener("click", () => {
+      navMenu.classList.toggle("active");
+      menuToggle.classList.toggle("open");
+    });
   }
 });
 // Script para iluminar o link da página atual automaticamente
-document.querySelectorAll('.nav-menu a').forEach(link => {
-    if (link.href === window.location.href) {
-        link.classList.add('active-link');
-    }
+document.querySelectorAll(".nav-menu a").forEach((link) => {
+  if (link.href === window.location.href) {
+    link.classList.add("active-link");
+  }
+});
+
+const backToTopButton = document.getElementById("backToTop");
+
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    backToTopButton.style.display = "flex";
+    backToTopButton.style.alignItems = "center";
+    backToTopButton.style.justifyContent = "center";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+};
+
+backToTopButton.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth", // Roda suavemente até o topo
+  });
 });
