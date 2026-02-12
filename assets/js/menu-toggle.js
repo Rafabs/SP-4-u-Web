786fc5362f19504f1d02a3ae6a9a6d8c57f80c85
Rafabs <rafael_barbosa2@hotmail.com>
2026-02-12 01:12:38 -0300

    main
    Ajuste fino na navbar

--- assets/js/menu-toggle.js
+++ assets/js/menu-toggle.js
@@ -8,6 +8,10 @@ document.addEventListener("DOMContentLoaded", () => {
     menuToggle.classList.toggle("open"); 
   });
   }
-
-
+});
+// Script para iluminar o link da página atual automaticamente
+document.querySelectorAll('.nav-menu a').forEach(link => {
+    if (link.href === window.location.href) {
+        link.classList.add('active-link');
+    }
 });
