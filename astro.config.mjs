7a9da0f584581bf340697f3fe7a963706182bdec
Rafabs <rafael_barbosa2@hotmail.com>
2026-02-21 00:25:10 -0300

    main
    Correção de direcionamento e limpeza de cache para execução local

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ export default defineConfig({
   site: 'https://rafabs.github.io/SP-4-u-Web',
   base: '/SP-4-u-Web/',
   vite: {
+    cacheDir: "C:/temp/vite-cache", // Move o cache para fora do OneDrive
     build: {
       rollupOptions: {
         output: {
