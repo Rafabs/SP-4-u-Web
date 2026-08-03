8acaad756186607dd4cb9271616af1abd210869c
Rafabs <rafael_barbosa2@hotmail.com>
2026-08-03 01:07:17 -0300

    main
    v26.08.1 Atualização dos dados de Mapa do Citylines.co com base no GEOSampa

--- src/data/roadmap.ts
+++ src/data/roadmap.ts
@@ -51,6 +51,7 @@ const roadmapData: Roadmap = {
     },
   ],
   historico: [
+    { data: "03/08/2026", versao: "v26.08.1", descricao: "Atualização dos dados de Mapa do Citylines.co com base no GEOSampa" },    
     { data: "29/07/2026", versao: "v26.07.15", descricao: "Criação de itens GTFS da ARTESP com adaptação de históricos, atualização GTFS - SPTrans e Inserção de cores hexadecimal das linhas 6 e 17" },    
     { data: "25/07/2026", versao: "v26.07.14", descricao: "Atualização GTFS - SPTrans e (GTFS): Previne erro ao ler shapes com linhas vazias ou indefinidas" },    
     { data: "22/07/2026", versao: "v26.07.13", descricao: "Correção de estilos de Detalhes na versão mobile" },    
