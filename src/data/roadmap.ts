e6c14bda45a6cacfdcc5dd16757478aae30abbb6
Rafabs <rafael_barbosa2@hotmail.com>
2026-08-20 16:39:33 -0300

    main
    v26.08.4 Atualização GTFS - SPTrans e Atualização de ícones do Mapa

--- src/data/roadmap.ts
+++ src/data/roadmap.ts
@@ -51,6 +51,7 @@ const roadmapData: Roadmap = {
     },
   ],
   historico: [
+    { data: "20/08/2026", versao: "v26.08.5", descricao: "Atualização GTFS - SPTrans e Atualização de ícones do Mapa" },    
     { data: "18/08/2026", versao: "v26.08.4", descricao: "Atualização GTFS - SPTrans" },    
     { data: "12/08/2026", versao: "v26.08.3", descricao: "Atualização GTFS - SPTrans e Correção de erro no carregamento de status das linhas" },    
     { data: "09/08/2026", versao: "v26.08.2", descricao: "Atualização GTFS - SPTrans e Correção de erro no carregamento de status das linhas" },    
