cc24c6a5dedf9e56c514f6ff34a68101857c3b15
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2026-09-13 18:34:36 -0300

    main
    v26.09.4 Correção de itens em algumas páginas

--- src/data/roadmap.ts
+++ src/data/roadmap.ts
@@ -51,6 +51,7 @@ const roadmapData: Roadmap = {
     },
   ],
   historico: [
+    { data: "13/09/2026", versao: "v26.09.4", descricao: "Correção de itens em algumas páginas" },    
     { data: "12/09/2026", versao: "v26.09.3", descricao: "Atualização do termo Operação Diferenciada em status de linha" },    
     { data: "11/09/2026", versao: "v26.09.2", descricao: "Atualização GTFS - SPTrans" },    
     { data: "06/09/2026", versao: "v26.09.1", descricao: "Atualização de informações, seguindo o ESCOPO REGULATÓRIO da ARTESP" },    
