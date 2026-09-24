985acfba7b49e485afdf92314e7705a4fd07eb9d
Rafabs <rafael_barbosa2@hotmail.com>
2026-09-24 18:25:32 -0300

    main
    v26.09.6 Atualização GTFS - SPTrans

--- src/data/roadmap.ts
+++ src/data/roadmap.ts
@@ -51,6 +51,7 @@ const roadmapData: Roadmap = {
     },
   ],
   historico: [
+    { data: "24/09/2026", versao: "v26.09.6", descricao: "Atualização GTFS - SPTrans" },    
     { data: "20/09/2026", versao: "v26.09.5", descricao: "Atualização GTFS - SPTrans" },    
     { data: "13/09/2026", versao: "v26.09.4", descricao: "Correção de itens em algumas páginas" },    
     { data: "12/09/2026", versao: "v26.09.3", descricao: "Atualização do termo Operação Diferenciada em status de linha" },    
